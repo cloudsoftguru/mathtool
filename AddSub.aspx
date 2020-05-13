3a2524f0733405e1f36ecddd841961124394200d
cloudsoftguru <65293035+cloudsoftguru@users.noreply.github.com>
2020-05-13 19:00:56 +0530

    Practice Addition and Subtraction

---
+++ AddSub.aspx
@@ -0,0 +1,138 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ADRIJA/MasterPageWhatsApp.master" AutoEventWireup="true" CodeFile="AddSub.aspx.cs" Inherits="ADRIJA_AddSub" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+    <script>
+        var a = 0;
+        setInterval(function () {
+            a = a + 1;
+            if (a > 0) {
+                document.getElementById('<%=Image1.ClientID%>').setAttribute('src', 'http://math.mycomputech.in/Adrija/img/normal.png');
+                a = 0;
+            }
+        }, 4000);
+        function getres() {
+            var result = document.getElementById('<%=TextBox3.ClientID%>').value
+            var num1 = document.getElementById('<%=TextBox1.ClientID%>').value
+            var num2 = document.getElementById('<%=TextBox2.ClientID%>').value
+            if (document.getElementById('<%=Label2.ClientID%>').innerText == '+') {
+
+                if (parseInt(result, 10) == (parseInt(num1, 10) + parseInt(num2, 10))) { var msg = new SpeechSynthesisUtterance('Thats correct'); window.speechSynthesis.speak(msg); }
+                else { var msg = new SpeechSynthesisUtterance('That was wrong'); window.speechSynthesis.speak(msg); }
+            }
+            else {
+                if (parseInt(result, 10) == (parseInt(num1, 10) - parseInt(num2, 10))) { var msg = new SpeechSynthesisUtterance('Thats correct'); window.speechSynthesis.speak(msg); }
+                else { var msg = new SpeechSynthesisUtterance('That was wrong'); window.speechSynthesis.speak(msg); }
+            }
+        }
+
+    </script>
+    <style type="text/css">
+        .img1:hover {
+            /*Start the shake animation and make the animation last for 0.5 seconds*/
+            animation: shake 0.5s;
+            /*When the animation is finished, start again*/
+            animation-iteration-count: infinite;
+        }
+
+        @keyframes shake {
+            0% {
+                transform: translate(1px, 1px) rotate(0deg);
+            }
+
+            10% {
+                transform: translate(-1px, -2px) rotate(-1deg);
+            }
+
+            20% {
+                transform: translate(-3px, 0px) rotate(1deg);
+            }
+
+            30% {
+                transform: translate(3px, 2px) rotate(0deg);
+            }
+
+            40% {
+                transform: translate(1px, -1px) rotate(1deg);
+            }
+
+            50% {
+                transform: translate(-1px, 2px) rotate(-1deg);
+            }
+
+            60% {
+                transform: translate(-3px, 1px) rotate(0deg);
+            }
+
+            70% {
+                transform: translate(3px, 1px) rotate(-1deg);
+            }
+
+            80% {
+                transform: translate(-1px, -1px) rotate(1deg);
+            }
+
+            90% {
+                transform: translate(1px, 2px) rotate(0deg);
+            }
+
+            100% {
+                transform: translate(1px, -2px) rotate(-1deg);
+            }
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <div class="col-12 col-s-12">
+                <p style="font-family: Helvetica Neue,Helvetica,Arial,sans-serif; font-size: 40px;">
+                    
+                    
+                        <asp:TextBox ID="TextBox5" runat="server" CssClass="form-control-inline" Style="text-align: center" Text="10" TextMode="Number" Width="90px" />
+                        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="START" />
+                    
+                    <span style="float:right">
+                        Marks:
+                    <asp:Label ID="Label1" runat="server" Text="0" ForeColor="#000099" Font-Bold="True"></asp:Label>
+                    &nbsp;&nbsp;&nbsp;Correct:
+                    <asp:Label ID="Label3" runat="server" Text="0" ForeColor="#006600"></asp:Label>
+                    &nbsp;&nbsp;&nbsp;Wrong:
+                    <asp:Label ID="Label4" runat="server" Text="0" ForeColor="Red"></asp:Label>
+                    </span>
+                        
+                    <hr />
+                    <asp:TextBox ID="TextBox1" runat="server" AutoPostBack="True" onchange="getres()" CssClass="form-control-inline" Font-Size="50" Height="200" OnTextChanged="TextBox1_TextChanged" Style="text-align: center" TextMode="Number" Width="20%"></asp:TextBox>
+                    <span style="font-family: Helvetica Neue,Helvetica,Arial,sans-serif; font-size: 100px;">
+                        <asp:Label ID="Label2" runat="server" Font-Size="100" Style="padding: 40px" Text="+"></asp:Label>
+                    </span>
+                    <asp:TextBox ID="TextBox2" runat="server" AutoPostBack="True" onchange="getres()" CssClass="form-control-inline" Font-Size="50" Height="200" OnTextChanged="TextBox2_TextChanged" Style="text-align: center" TextMode="Number" Width="20%"></asp:TextBox>
+                    <span style="font-family: Helvetica Neue,Helvetica,Arial,sans-serif; font-size: 100px; padding: 40px">= </span>
+                    <asp:TextBox ID="TextBox3" runat="server" AutoPostBack="True" onchange="getres()" CssClass="form-control-inline" Font-Size="50" Height="200" OnTextChanged="TextBox3_TextChanged" Style="text-align: center" TextMode="Number" Width="20%"></asp:TextBox>
+                    <hr />
+                    <p style="text-align: center">
+                        <asp:image ID="Image1" class="img1" runat="server" Width="300px" Height="300px" ImageUrl="~/ADRIJA/img/normal.png" />
+                    </p>
+
+                    <span style="float:left; vertical-align:central">
+
+                    <asp:label ID="Label7" runat="server" Font-Size="50" Style="padding: 20px" Text=""/>
+                    <asp:label ID="Label10" runat="server" Font-Size="50" Style="padding: 20px" Text="" ForeColor="#339933"/>
+                    <asp:label ID="Label5" runat="server" Font-Size="50" Style="padding: 20px" Text=""/>
+                    <asp:label ID="Label8" runat="server" Font-Size="50" Style="padding: 20px" Text=""/>
+                    <asp:label ID="Label11" runat="server" Font-Size="50" Style="padding: 20px" Text="" ForeColor="#339933"/>
+                    <asp:label ID="Label13" runat="server" Font-Size="50" Style="padding: 20px" Text=""/>
+                    <asp:label ID="Label9" runat="server" Font-Size="50" Style="padding: 20px" Text=""/>
+                    <asp:label ID="Label6" runat="server" Visible="false" Text=""/>
+                    <asp:label ID="Label12" runat="server" Font-Size="50" Style="padding: 20px" Text="" ForeColor="#339933"/>
+                       <asp:image ID="Image2" Visible="false" runat="server" Width="50px" Height="50px" />
+                        </span> 
+                </p>
+
+            </div>
+
+        </ContentTemplate>
+    </asp:UpdatePanel>
+
+</asp:Content>
+
---
+++ AddSub.aspx.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ADRIJA_AddSub : System.Web.UI.Page
+{
+    int range = 10;
+    int oper;
+    int pos;
+    int correctResult;
+   
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        range =int.Parse(TextBox5.Text);
+    }
+    private void formQuestion()
+    {
+        Random r = new Random();
+        oper = r.Next(1, 3);
+       
+        if (oper == 1)
+        {
+            Label2.Text = "+";
+        }
+        else { Label2.Text = "-"; }
+
+        pos = r.Next(1, 4);
+
+        switch (pos)
+
+        {
+            case 1:
+                resetfield();
+                TextBox1.BackColor = System.Drawing.Color.Yellow;
+                TextBox2.BackColor = System.Drawing.Color.GreenYellow;
+                TextBox3.BackColor = System.Drawing.Color.GreenYellow;
+                TextBox2.Enabled = false;
+                TextBox3.Enabled = false;
+                setNum(pos, oper);
+                TextBox1.Focus();
+                break;
+            case 2:
+                resetfield();
+                TextBox2.BackColor = System.Drawing.Color.Yellow;
+                TextBox1.BackColor = System.Drawing.Color.GreenYellow;
+                TextBox3.BackColor = System.Drawing.Color.GreenYellow;
+                TextBox1.Enabled = false;
+                TextBox3.Enabled = false;
+                setNum(pos, oper);
+                TextBox2.Focus();
+                break;
+            default:
+                resetfield();
+                TextBox3.BackColor = System.Drawing.Color.Yellow;
+                TextBox1.BackColor = System.Drawing.Color.GreenYellow;
+                TextBox2.BackColor = System.Drawing.Color.GreenYellow;
+                TextBox1.Enabled = false;
+                TextBox2.Enabled = false;
+                setNum(pos, oper);
+                TextBox3.Focus();
+
+                break;
+        }
+
+    }
+    private void setNum(int a,int op)
+    {
+        
+        Random r = new Random();
+        int num1;
+        int num2;
+        Boolean ok = false;
+        switch (a)
+
+        {
+            case 1:
+                do
+                {
+                    num1 = r.Next(0, range);
+                    num2 = r.Next(0, range);
+                    if (op == 1)
+                    {
+                        if (num2 > num1) { ok = true; correctResult = num2 - num1; }
+                    }
+                    else
+                    {
+                        ok = true;
+                        correctResult = num2 + num1;
+                    }
+                    
+                } while (!ok);
+
+                TextBox2.Text = num1.ToString();
+                TextBox3.Text = num2.ToString();
+               
+                break;
+            case 2:
+                do
+                {
+                    num1 = r.Next(0, range);
+                    num2 = r.Next(0, range);
+                    if (op == 1)
+                    {
+                        if (num2 > num1) { ok = true; correctResult = num2 - num1; }
+                    }
+                    else
+                    {
+                        if (num1 > num2) { ok = true; correctResult = num1 - num2; }
+                    }
+
+                } while (!ok);
+                TextBox1.Text = num1.ToString();
+                TextBox3.Text = num2.ToString();
+                
+                break;
+            default:
+                do
+                {
+                    num1 = r.Next(0, range);
+                    num2 = r.Next(0, range);
+                    if (op == 1)
+                    {
+                        ok = true;
+                        correctResult = num2 + num1;
+                    }
+                    else
+                    {
+                        if (num1 > num2) { ok = true; correctResult = num1 - num2; }
+                    }
+
+                } while (!ok);
+
+                TextBox1.Text = num1.ToString();
+                TextBox2.Text = num2.ToString();
+               
+                break;
+
+        }
+        Label6.Text = correctResult.ToString();
+
+
+    }
+    private void resetfield()
+    {
+        TextBox1.Enabled = true;
+        TextBox2.Enabled = true;
+        TextBox3.Enabled = true;
+        TextBox1.Text = "";
+        TextBox2.Text = "";
+        TextBox3.Text = "";
+        TextBox1.BackColor = System.Drawing.Color.White;
+        TextBox2.BackColor = System.Drawing.Color.White;
+        TextBox3.BackColor = System.Drawing.Color.White;
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        formQuestion();
+        Image2.Visible = true;
+    }
+
+   
+    private void resultEffect()
+    {
+       
+
+        if (getResult())
+        {
+            Label1.Text = (int.Parse(Label1.Text) + 1).ToString();
+            Label3.Text = (int.Parse(Label3.Text) + 1).ToString();
+            Image1.ImageUrl = "/Adrija/img/sm.png";
+            Image2.ImageUrl = "/Adrija/img/check.jpg";
+        }
+        else
+        {
+            Label1.Text = (int.Parse(Label1.Text) - 1).ToString();
+            Label4.Text = (int.Parse(Label4.Text) + 1).ToString();
+            Image1.ImageUrl = "/Adrija/img/sad.png";
+            Image2.ImageUrl = "/Adrija/img/cross.png";
+        }
+
+      
+        
+    }
+
+    private int resetImage()
+    {
+        //To be called from an async process
+        //Task<int> task = new Task<int>(resetImage);
+        //task.Start();
+        //int count = await task;
+
+        Thread.Sleep(3000);
+        Image1.ImageUrl = "/Adrija/img/sad.png";
+        return 0;
+    }
+   
+
+    public Boolean getResult()
+    {
+        Boolean result = false;
+
+        Label7.Text = TextBox1.Text;
+        Label8.Text = TextBox2.Text;
+        Label9.Text = TextBox3.Text;
+        Label5.Text = Label2.Text;
+        Label13.Text = "=";
+
+       // Label6.Text = correctResult.ToString();
+
+        int rlt = Int32.Parse(TextBox3.Text);
+        if (Label2.Text=="+")
+        {
+            if(rlt == (Int32.Parse(TextBox1.Text) + Int32.Parse(TextBox2.Text)) )
+            {
+                result = true;
+               
+            }
+           
+        }
+        else
+        {
+            if (rlt == (Int32.Parse(TextBox1.Text) - Int32.Parse(TextBox2.Text)))
+            {
+                result = true;
+               
+            }
+           
+            //Label6.Text = (Int32.Parse(TextBox4.Text) - Int32.Parse(TextBox6.Text)).ToString();
+        }
+
+        
+        return result;
+    }
+
+
+
+    
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+        Label10.Text = "(" + Label6.Text + ")";
+        Label11.Text = "";
+        Label12.Text = "";
+        resultEffect();
+        formQuestion();
+        
+
+    }
+
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+        Label11.Text = "(" + Label6.Text + ")";
+        Label10.Text = "";
+        Label12.Text = "";
+        resultEffect();
+        formQuestion();
+       
+    }
+
+    protected void TextBox3_TextChanged(object sender, EventArgs e)
+    {
+        Label12.Text = "(" + Label6.Text + ")";
+        Label10.Text = "";
+        Label11.Text = "";
+
+        resultEffect();
+        formQuestion();
+       
+
+    }
+  
+}
